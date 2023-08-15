ac8ef7c4419ce932f796b6a854ba9828428902be
Ivy Jackson <34287279+ivyjsgit@users.noreply.github.com>
2023-08-15 09:17:37 -0500

    Initial Commit

---
+++ SystemMonitor/SystemMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemMonitorApp.swift
+//  SystemMonitor
+//
+//  Created by Ivy Jackson on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SystemMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
